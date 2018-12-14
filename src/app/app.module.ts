eb26d7deb1208e2cc4b7d4921c6e1bf255ffd9ce
Elerkun <42411047+Elerkun@users.noreply.github.com>
2018-12-15 00:54:37 +0100

    add footer, text: Manten tus Animes y mangas al dia,Comparte con tus amigos,Animes Recomendados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule
