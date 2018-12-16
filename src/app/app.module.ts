9c65f477ace7e8ac3ffeb46a4d14f416038d7c31
Elerkun <42411047+Elerkun@users.noreply.github.com>
2018-12-17 00:04:54 +0100

    add routeModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
 
-
+const routes: Routes = [
+  {path: '', redirectTo: '/index', pathMatch: 'full'},
+  {path:'index', component:IndexComponent},
+  {path:'login', component:LoginComponent},
+]
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     FooterComponent,
     LoginComponent,
-    
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
